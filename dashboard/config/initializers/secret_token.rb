8921e2d801d63f914f4e772b4bf93a1e705355c6
Michel Grootjans <michel.grootjans@gmail.com>
2015-03-29 16:50:32 +0200

    created dashboard app

---
+++ dashboard/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dashboard::Application.config.secret_key_base = '7179d20b8ba26c3b1181cbb183d9f6d8146b994132b92720f04bb1a4ce1a8e429a111c317c6caf1fa2701e808e1a71571d692713404740de59d4a7fbaa488fee'
